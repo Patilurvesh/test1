99249e6b44e66dae76c94a5685fd59fb6a803bd1
Patilurvesh <101618211+Patilurvesh@users.noreply.github.com>
2022-03-28 11:19:55 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,24 @@
+package pack;
+
+import java.util.Scanner;
+
+public class Q3 {
+
+	public static void main(String[] args)
+	{
+		int n1,n2,sum;
+		
+		 Scanner scan = new Scanner(System.in);
+	       System.out.println("Chech the sum of two number");
+	      
+	       System.out.println("Enter the number n1");
+	       n1=scan.nextInt();
+	       System.out.println("Enter the number n2");
+	       n2=scan.nextInt();
+	       
+	       sum=n1+n2;
+	       System.out.println("Sum of two number is ="+sum);
+
+	}
+
+}
