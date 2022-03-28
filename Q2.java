9a38446b849a75307e75d0e03e771033824a8a72
Patilurvesh <101618211+Patilurvesh@users.noreply.github.com>
2022-03-28 11:19:33 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,18 @@
+package pack;
+
+
+public class Q2 {
+
+	public static void main(String[] args) 
+	{
+       int n1=10;
+    		 int n2=40,sum;
+       
+       
+       sum=n1+n2;
+       System.out.println("Sum of two number is = "+sum);
+    
+ 
+	}
+
+}
